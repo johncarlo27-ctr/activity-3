179499957b84fdb9760f4d9e332ca5a3b672ee1b
unknown <johncarloaltura705@gmail.com>
2024-09-10 15:02:54 +0800

    Completed Variables Section

---
+++ number.js
@@ -0,0 +1,21 @@
+const firstfavnum = 9;
+const secondfavnum = 6;
+
+//Addition
+const sum = firstfavnum + secondfavnum;
+console.log(sum);
+//Subtraction 
+const difference = firstfavnum - secondfavnum;
+console.log(difference);
+//Multiplication 
+const product = firstfavnum * secondfavnum;
+console.log(product);
+//Divission 
+const qoutient = firstfavnum / secondfavnum;
+console.log(qoutient);
+//Modulus
+const remainder = firstfavnum % secondfavnum;
+console.log(remainder);
+//Exponentiation(power)
+const power = firsfavnum ** secondfavnum; 
+console.log(power);
